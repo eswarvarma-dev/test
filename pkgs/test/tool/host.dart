14511b2447997230c9fc4f55a2ac0b7eb7f0be03
Yegor Jbanov <yjbanov@google.com>
2019-10-23 10:49:19 -0700

    instrument host.dart

--- pkgs/test/tool/host.dart
+++ pkgs/test/tool/host.dart
@@ -120,6 +120,10 @@ void main() {
       if (message['command'] == 'loadSuite') {
         var suiteChannel =
             serverChannel.virtualChannel(message['channel'] as int);
+        suiteChannel.sink.add({
+          'type': 'print',
+          'line': '>>> hello from host.dart',
+        });
         var iframeChannel =
             _connectToIframe(message['url'] as String, message['id'] as int);
         suiteChannel.pipe(iframeChannel);
